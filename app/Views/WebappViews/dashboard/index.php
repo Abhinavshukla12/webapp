ae3352524d209ac4ce81c81615020747ea53069f
Abhinav shukla <124790218+Abhinavshukla12@users.noreply.github.com>
2024-05-29 21:32:38 +0530

    creating home page content

--- app/Views/WebappViews/dashboard/index.php
+++ app/Views/WebappViews/dashboard/index.php
@@ -1,5 +1,30 @@
 <?= $this->extend('WebappViews/layout/default') ?>
 
 <?= $this->section('content') ?>
-<h1>home</h1>
+<div class="container">
+    <div class="jumbotron mt-4">
+        <h1 class="display-4">Welcome to Our Website!</h1>
+        <p class="lead">This is the homepage of our web application built with CodeIgniter 4.5.</p>
+        <hr class="my-4">
+        <p>Explore the features and functionalities we offer.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Feature One</h2>
+            <p>Details about the first feature. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+            <h2>Feature Two</h2>
+            <p>Details about the second feature. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+            <h2>Feature Three</h2>
+            <p>Details about the third feature. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+    </div>
+</div>
 <?= $this->endSection() ?>
